bab0ed79daa1f3ca09cf128cd176a3ebef0b3abf
Timm-Osorio <162040931+timeqx@users.noreply.github.com>
2025-07-29 16:42:13 +0800

    Update Footer.svelte

--- src/lib/sections/Footer.svelte
+++ src/lib/sections/Footer.svelte
@@ -5,7 +5,7 @@
 <footer class="bg-white text-black py-2 md:px-6">
     <div class="md:max-w-6xl mx-auto text-center">
       <p class="text-gray-900 font-[plexMono] text-[0.65rem] md:text-sm uppercase">
-        © 2024 {profile.name}. Built with SvelteKit, GSAP, & Tailwind CSS.
+        © 2025 {profile.name}. All rights reserved.
       </p>
     </div>
   </footer>
